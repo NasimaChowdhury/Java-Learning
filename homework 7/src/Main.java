39ed25d25a1ff449658e7f53c0e53f81b6dda36d
NasimaChowdhury <nsmchwdhry@gmail.com>
2023-08-11 11:26:45 -0400

    Variables type in Java
    Variables type in Java

---
+++ homework 7/src/Main.java
@@ -0,0 +1,18 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        String name = "'Jhon'" ;
+        int age = 18 ;
+        float height = 5.9f ;
+        double yearlySalary = 16.000 ;
+        double accountBalance = 3.500 ;
+        int grade = 12 ;
+        char gender = 'M' ;
+        boolean student = true ;
+        System.out.println("Student Information" +"\n Name = " + name + "\n Age = " + age + "\n Height = "
+                + height + "\n Yearly Salary = $" + yearlySalary + "\n Account Balance = $" + accountBalance +
+                 "\n Grade = " + grade + "\n Gender = " + gender + "\n Student = " + student );
+
+        }
+    }
