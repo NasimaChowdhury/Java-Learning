b9016e1e65de3db90c7d66fcd5920cdbc9327860
NasimaChowdhury <nsmchwdhry@gmail.com>
2023-08-11 11:50:57 -0400

    HW 6 done
    HW 6 Done

---
+++ homework 6/src/Main.java
@@ -0,0 +1,14 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+       String name = "Jhon Doe" ;
+        String registionNumber = "239A874" ;
+        int age = 18 ;
+        String address = "yyyyyyyyyyyyyyy" ;
+        Double cgpa = 3.89 ;
+        System.out.println("Here is the Student details \n"+  "Name = " + name + "\nRegistionNumber = "+ registionNumber
+                +"\nAge = " + age +"\nAddress = " + address +"\nCGPA = "+ cgpa );
+
+    }
+}
